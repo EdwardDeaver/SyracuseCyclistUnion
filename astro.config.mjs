4542da87831ec5aabb685fb75f49e0197ea20800
Edward C. Deaver, IV <EdwardDeaver@users.noreply.github.com>
2024-08-29 19:56:34 -0400

    partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown'
+
+import sitemap from '@astrojs/sitemap';
+// https://astro.build/config
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://syracusecyclistunion.org/',
+    integrations: [
+        sitemap(),
+        partytown({
+            config: {
+              forward: ["dataLayer.push"],
+            },
+        }),
+    ],
+});
